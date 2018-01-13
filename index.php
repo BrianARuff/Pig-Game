d42abe38d04ce663f47954f6a5dd68c9e5e99c13
Brian Ruff <thelonewolf198927@gmail.com>
2018-01-12 22:31:02 -0500

    pigs

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include_once('home.html ') ?>
+<?php include_once('home.html') ?>
+<?php include_once('app.js') ?>
+<?php include_once('dice-1.png') ?>
+<?php include_once('dice-2.png') ?>
+<?php include_once('dice-3.png') ?>
+<?php include_once('dice-4.png') ?>
+<?php include_once('dice-5.png') ?>
+<?php include_once('dice-6.png') ?>

82a9e49189e3e5d15eea77c67012df8136756301
Brian Ruff <thelonewolf198927@gmail.com>
2018-01-12 22:40:13 -0500

    php pig game

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php include_once('home.html') ?>
+<?php include_once('style.css') ?>
 <?php include_once('app.js') ?>
-<?php include_once('dice-1.png') ?>
-<?php include_once('dice-2.png') ?>
-<?php include_once('dice-3.png') ?>
-<?php include_once('dice-4.png') ?>
-<?php include_once('dice-5.png') ?>
-<?php include_once('dice-6.png') ?>
-<?php include_once('mountain.jpg') ?>
